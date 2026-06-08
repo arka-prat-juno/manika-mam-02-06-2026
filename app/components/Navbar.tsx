f7eb848c253788f87fb6537200cecee362649aac
Arka Pratim Chaudhuri <arka.chaudhuri@junomoneta.in>
2026-06-08 15:39:47 +0530

    re

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,53 +1,38 @@
-import {
-    NavLink 
-} from "react-router";
+import { NavLink } from "react-router";
 import styles from "./navbar.module.css";
 
 export function Navbar() {
     return (
-        <nav className={styles.navbar}>
-            {/* Logo removed */}
-            <div className={styles.links}>
-                <NavLink
-                    to="/dashboard/trades"
-                    className={styles.link}
-                >
+        <aside className={styles.sidebar}>
+            <div className={styles.logo}>
+                Stockwise
+            </div>
+
+            <nav className={styles.links}>
+                <NavLink to="/dashboard/trades" className={styles.link}>
                     My Trades
                 </NavLink>
 
-                <NavLink
-                    to="/dashboard/mtm"
-                    className={styles.link}
-                >
+                <NavLink to="/dashboard/mtm" className={styles.link}>
                     Stockwise Summary
                 </NavLink>
-                <NavLink
-                    to="/dashboard/calendar-pnl"
-                    className={styles.link}
-                >
+
+                <NavLink to="/dashboard/calendar-pnl" className={styles.link}>
                     Calendar PNL
                 </NavLink>
 
-                <NavLink
-                    to="/dashboard/add-trade"
-                    className={styles.link}
-                >
+                <NavLink to="/dashboard/add-trade" className={styles.link}>
                     Add Trade
                 </NavLink>
-                {/* manage-users */}
-                <NavLink
-                    to="/dashboard/manage-users"
-                    className={styles.link}
-                >
+
+                <NavLink to="/dashboard/manage-users" className={styles.link}>
                     Manage Users
                 </NavLink>
-                <NavLink
-                    to="/logout"
-                    className={styles.link}
-                >
+
+                <NavLink to="/logout" className={styles.link}>
                     Logout
                 </NavLink>
-            </div>
-        </nav>
+            </nav>
+        </aside>
     );
 }
